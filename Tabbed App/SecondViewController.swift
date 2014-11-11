fc06e5dfd76a26cb5abb5c06cabf714fb3b289c3
hminh1208 <hminh1208@gmail.com>
2014-11-12 00:23:09 +0700

    add to repo

--- Tabbed App/SecondViewController.swift
+++ Tabbed App/SecondViewController.swift
@@ -13,6 +13,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("Second View loaded")
     }
 
     override func didReceiveMemoryWarning() {
